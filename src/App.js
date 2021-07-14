5cda61f524836402fd1a256590bc3da1e39715e0
jamienewbigging <jamienweb@gmail.com>
2021-07-14 10:42:03 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {MdClose} from "react-icons/md";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div id={'body'}>
+
+                <h1>TO DO LIST</h1>
+
+                <form id={'add'}>
+                    <label>
+                        Add new TO DO:
+                        <input id={'textin'} type={'text'} name={'new'}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+
+                <div>
+                    <input type={'checkbox'} name={'done'}/>
+                    <p>Example</p>
+                    <MdClose color={'red'}/>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
