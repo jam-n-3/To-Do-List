cd25132502a2757ed0bb20edb3710594cafe205c
jamienewbigging <jamienweb@gmail.com>
2021-07-14 12:27:57 +0100

    added add and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import './App.css';
 import {MdClose} from "react-icons/md";
+import {MdEdit} from "react-icons/md";
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {newTODO: '', TODOs: localStorage.length}
+    }
+
+    writeTODO = (event) => {
+        this.setState({newTODO: event.target.value});
+    }
+
+    submitTODO = (event) => {
+        this.setState({newTODO: ''});
+        if (localStorage.length > 0) {
+            localStorage.setItem((Math.max(...Object.keys(localStorage)) + 1).toString(), this.state.newTODO);
+        }
+        else {
+            localStorage.setItem('0', this.state.newTODO);
+        }
+        event.preventDefault();
+    }
+
+    deleteTODO = (key) => {
+        localStorage.removeItem(key);
+        this.setState({TODOs: localStorage.length});
+    }
+
     render() {
         return (
             <div id={'body'}>
 
                 <h1>TO DO LIST</h1>
 
-                <form id={'add'}>
+                <form id={'add'} onSubmit={this.submitTODO}>
                     <label>
                         Add new TO DO:
-                        <input id={'textin'} type={'text'} name={'new'}/>
+                        <input id={'textIn'} type={'text'} value={this.state.newTODO} onChange={this.writeTODO} />
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
 
-                <div>
-                    <input type={'checkbox'} name={'done'}/>
-                    <p>Example</p>
-                    <MdClose color={'red'}/>
+                <div id={'list'}>
+                    {Object.keys(localStorage).map((obj)=>(
+                        <div className={'entry'}>
+                            <p className={'text'}>{localStorage.getItem(obj)}</p>
+                            <MdEdit className={'edit'} color={'blue'} />
+                            <MdClose className={'delete'} color={'red'} onClick={()=>this.deleteTODO(obj)} />
+                        </div>
+                    ))}
                 </div>
 
             </div>
